a97dc9e78a2e41e8b1036d032f6a9feb51a6e336
IIpocToIIapeHb <kostiarudoy2303@gmail.com>
2022-04-06 14:29:16 +0300

    Six commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,10 @@
 public class Main {
     public static void main(String[] args) {
+
+
         XMLValidator validator = new XMLValidator("data_xml/bankDeposits.xsd");
         System.out.println(validator.isValid("data_xml/bankDeposits.xml"));
+
+
     }
 }
